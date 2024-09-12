08c8e41df58c942c457c31cbe305401c4ec24975
219730 <2026margrave.asher@fcboe.org>
2024-09-12 11:08:41 -0400

    worked on vig_encode

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -3,7 +3,20 @@
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def vig_encode(text, keyword):
-  return ""
+  new_str = ""
+  keyword_list = []
+  for letter in text:
+    index = alpha.find(letter)
+    #print(index)
+    for let in keyword:
+      move = 0
+      keyword_list.append(let)
+      index2 = alpha.find(keyword_list[move])
+      move += 1
+
+    print(index2)
+
+
 
 
 def vig_decode(text, keyword):
@@ -13,7 +26,7 @@ def vig_decode(text, keyword):
 test = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
 vig_key = "TEST"
 enc = vig_encode(test, vig_key)
-dec = vig_decode(enc, vig_key)
+#dec = vig_decode(enc, vig_key)
 print(enc)
-print(dec)
+#print(dec)
 # If this worked, dec should be the same as test!
