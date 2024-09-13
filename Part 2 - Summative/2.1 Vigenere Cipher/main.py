8de9b4700989c8951661fab9e9ec077b2f869747
219730 <2026margrave.asher@fcboe.org>
2024-09-13 11:05:31 -0400

    Fixed the move variable in vig_encode

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -1,22 +1,17 @@
-# Read the instructions to see what you need to do here!
-
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+move = 0
 def vig_encode(text, keyword):
+  global index2
   new_str = ""
   keyword_list = []
   for letter in text:
     index = alpha.find(letter)
-    #print(index)
-    for let in keyword:
-      move = 0
-      keyword_list.append(let)
-      index2 = alpha.find(keyword_list[move])
-      move += 1
-
-    print(index2)
-
-
+  for let in keyword:
+    global move
+    keyword_list.append(let)
+    index2 = alpha.find(keyword_list[move])
+    move += 1
 
 
 def vig_decode(text, keyword):
