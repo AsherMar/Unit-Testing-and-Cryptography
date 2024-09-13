faa98784df8fb531ad5f3e6f5e1daf246d4bc203
219730 <2026margrave.asher@fcboe.org>
2024-09-13 11:10:11 -0400

    worked on finding letter to output

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -5,14 +5,16 @@ def vig_encode(text, keyword):
   global index2
   new_str = ""
   keyword_list = []
-  for letter in text:
-    index = alpha.find(letter)
   for let in keyword:
     global move
     keyword_list.append(let)
     index2 = alpha.find(keyword_list[move])
     move += 1
-
+  for letter in text:
+    index = alpha.find(letter)
+    index3 = index + index2
+    if index3 > len(alpha):
+      index3 %= len(alpha)
 
 def vig_decode(text, keyword):
   return ""
