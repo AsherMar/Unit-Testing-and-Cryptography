ce9cbe52b88fefdac8a2f1b7320ef7acd96c612d
219730 <2026margrave.asher@fcboe.org>
2024-09-18 11:10:46 -0400

    basically did nothing

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -5,18 +5,18 @@ def vig_encode(text, keyword):
   global index2
   new_str = ""
   keyword_list = []
-  for let in keyword:
+  for letter in keyword:
     global move
-    keyword_list.append(let)
+    keyword_list.append(letter)
     index2 = alpha.find(keyword_list[move])
     move += 1
   for letter in text:
     index = alpha.find(letter)
     index3 = index + index2
     if index3 > len(alpha):
-      index3 %= 26
-      new_str += alpha[index3]
+      new_str += alpha[index3 % 26]
   return new_str
+  move = 0
 
 def vig_decode(text, keyword):
   return ""
