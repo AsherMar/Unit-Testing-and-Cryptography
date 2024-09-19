b7e33f3ee9bf2523fa028c5c8ee6396e7517e97f
219730 <2026margrave.asher@fcboe.org>
2024-09-19 11:10:55 -0400

    started working on vig_decode

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -19,13 +19,26 @@ def vig_encode(text, keyword):
   move = 0
 
 def vig_decode(text, keyword):
-  return ""
-
+  global index2
+  new_str = ""
+  keyword_list = []
+  for letter in keyword:
+    global move
+    keyword_list.append(letter)
+    index2 = alpha.find(keyword_list[move])
+    move += 1
+  for letter in text:
+    index = alpha.find(letter)
+    index3 = index + index2
+    if index3 > len(alpha):
+      new_str += alpha(index3 % 26)
+  return new_str
+  move = 0
 
 test = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
 vig_key = "TEST"
 enc = vig_encode(test, vig_key)
-#dec = vig_decode(enc, vig_key)
+dec = vig_decode(enc, vig_key)
 print(enc)
-#print(dec)
+print(dec)
 # If this worked, dec should be the same as test!
